bf35c0e368f5e50b2fc3e227ba1643d0143bac9c
Remmington Pascone <remmingtonpearce@gmail.com>
2021-02-25 14:59:53 -0800

    finished starter code for dataarea

--- employeedirectory/src/components/DataTable.js
+++ employeedirectory/src/components/DataTable.js
@@ -1,24 +1,38 @@
-import React from "react"; 
-import DataBody from "./DataBody"; 
-import "../styles/DataTable.css"; 
+import React from "react";
+import DataBody from "./DataBody";
+import "../styles/DataTable.css";
 
-function DataTable({ headings, users, handleSort }) { 
+function DataTable({ headings, users, handleSort }) {
     return (
         <div className="datatable mt-5">
             <table
                 id="table"
                 className="table tavble-striped table-hover table-">
-                    <thead>
-                        <tr>
-                            {headings.map(({ name, width }) => {
-                                return (
-                                    <th
+                <thead>
+                    <tr>
+                        {headings.map(({ name, width }) => {
+                            return (
+                                <th
                                     className="col"
-                                )
-                            })}
+                                    key={name}
+                                    style={{ width }}
+                                    onClick={() => { 
+                                        handleSort(name.toLowerCase()); 
+                                    }}
+                                    >
+                                        {name}
+                                        <span className="pointer"></span>
+                                        </th>
+                                );
+                        })}
                         </tr>
-                    </thead>
-                </table>
+                </thead>
+
+                <DataBody users={users} />
+            </table>
         </div>
-    )
+    );
 }
+
+
+export default DataTable;
---
+++ employeedirectory/src/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import DataArea from "./DataArea";
+
+
+function Main (){
+    return (
+        <>
+        <DataArea />
+        </>
+    );
+};
+
+
+export default Main;
